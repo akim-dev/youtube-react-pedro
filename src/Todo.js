29b7aecd48c950986f8259e007330e9a492a51f9
musta.akim <musta.akim@gmail.com>
2023-03-29 13:44:41 +0700

    komit kedua

---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+
+function Todo() {
+  return (
+    <div className="App">
+      <div className="addTask"></div>
+      <div className="list"></div>
+    </div>
+  );
+}
+
+export default Todo;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import App1 from "./App1";
+import Todo from "./Todo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <App1 />
+    {/* <App1 /> */}
+    <Todo />
   </React.StrictMode>
 );
